515fef5a5f489e4be3f0f26a18c41ab3eda55d07
Deák Gabriella <adeakgabi@gmail.com>
2019-01-17 15:21:32 +0100

    create DefineBasicInfo

---
+++ week-01/day-4/src/DefineBasicInfo.java
@@ -0,0 +1,22 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+
+        String name = "Deák Gabi";
+        int age = 26;
+        double height = 1.70;
+        boolean married = true;
+
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
+        System.out.println("My height is " + height);
+        System.out.println("Married? " + married);
+
+
+
+    }
+}
