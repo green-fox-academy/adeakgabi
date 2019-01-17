153659828e19dfc437fd035e77132138c36c7df6
Deák Gabriella <adeakgabi@gmail.com>
2019-01-17 14:08:31 +0100

    create introduce yourself

---
+++ week-01/day-4/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println( "Gabi Deák" );
+        System.out.println(26);
+        System.out.println(1.70);
+    }
+}
