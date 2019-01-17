23571166d69123759d504240ac46cfffa78c0a46
Deák Gabriella <adeakgabi@gmail.com>
2019-01-17 13:07:15 +0100

    create HelloOthers

---
+++ week-01/day-4/src/HelloOthers.java
@@ -0,0 +1,9 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+
+        System.out.println( "Hello, Dezső");
+        System.out.println("Hello, Viki");
+        System.out.println("Hello, Roni");
+
+    }
+}
