ab789fbcbc6eb5bf19e74d4af76bf0f68ef978db
Deák Gabriella <adeakgabi@gmail.com>
2019-01-18 14:18:36 +0100

    add DrawChessTable

---
+++ week-01/day-5/src/DrawChessTable.java
@@ -0,0 +1,30 @@
+public class DrawChessTable {
+    public static void main(String[] args) {
+
+        // Crate a program that draws a chess table like this
+        //
+        // % % % %
+        //  % % % %
+        // % % % %
+        //  % % % %
+        // % % % %
+        //  % % % %
+        // % % % %
+        //  % % % %
+        //
+        int a = 8;
+        for (int i = 0; i < a; i++) {
+            for (int k = 0; k < a; k++) {
+                if (i % 2 == 0 && k % 2 != 0) {
+                        System.out.print("%");
+                } else if (i % 2 != 0 && k % 2 == 0){
+                    System.out.print("%");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
