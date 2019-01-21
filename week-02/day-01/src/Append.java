3a97ef0f34b4c45ce986be07c0f1b70326c4def1
Deák Gabriella <adeakgabi@gmail.com>
2019-01-21 17:02:05 +0100

    add Colors and Append

---
+++ week-02/day-01/src/Append.java
@@ -0,0 +1,15 @@
+public class Append {
+    public static void main(String[] args) {
+        // - Create an array variable named `animals`
+        //   with the following content: `["koal", "pand", "zebr"]`
+        // - Add all elements an `"a"` at the end
+
+        String[]animals = {"koal", "pand", "zebr"};
+        for (int i =0; i < animals.length; i++){
+            String appendA = animals[i] +"a";
+            System.out.println(appendA);
+        }
+
+    }
+
+}
