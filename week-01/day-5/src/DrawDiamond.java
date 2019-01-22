b87cd2b5e15ed9581f46ce9a88f4a32e803a4973
Deák Gabriella <adeakgabi@gmail.com>
2019-01-22 11:52:40 +0100

    add some changes to DrawDiamond

--- week-01/day-5/src/DrawDiamond.java
+++ week-01/day-5/src/DrawDiamond.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class DrawDiamond {
     public static void main(String[] args) {
         // Write a program that reads a number from the standard input, then draws a
@@ -13,6 +14,18 @@ public class DrawDiamond {
         //    *
         //
         // The diamond should have as many lines as the number was
+
+      Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insert a number!");
+        int userInput = scanner.nextInt();
+
+        for(int i = 0; i < userInput; i++){
+            for (j = userInput -i; j > 1; j--){
+                System.out.print(" *");
+            }
+        }
+
     }
 
 
