9fcdaeda08b87bc10c328b87056e9091aad88635
Deák Gabriella <adeakgabi@gmail.com>
2019-01-23 14:15:59 +0100

    add PurpleSteps

---
+++ week-02/day-03/src/PurpleSteps.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps {
+    public static void mainDraw(Graphics graphics) {
+        // Reproduce this:
+        // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+
+        int x = 0;
+        int y = 0;
+        int size = 20;
+        for (int i = 0; i < 12; i++) {
+            drawSquare(x +(i*size), y +(i*size), size , graphics);
+
+        }
+
+
+    }public static void  drawSquare (int x, int y, int size, Graphics graphics){
+        graphics.setColor(new Color(100, 0, 100));
+        graphics.fill3DRect(x, y, size, size, true);
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
