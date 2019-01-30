e7dc009439ba18c1804a514771944f0cca8e25cc
Deák Gabriella <adeakgabi@gmail.com>
2019-01-30 12:13:09 +0100

    add Sharpie

---
+++ week-03/day-03/src/sharpie/SharpieObjects.java
@@ -0,0 +1,11 @@
+package sharpie;
+
+public class SharpieObjects {
+    public static void main(String[] args) {
+
+        Sharpie first = new Sharpie("black", 10.2);
+        first.use();
+        first.use();
+
+    }
+}
