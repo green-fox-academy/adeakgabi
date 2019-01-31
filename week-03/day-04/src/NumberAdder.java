2bb5d5d3137ac96a501bec7ba0f3094f85fa2edc
Deák Gabriella <adeakgabi@gmail.com>
2019-01-31 11:25:01 +0100

    add NumberAdder

---
+++ week-03/day-04/src/NumberAdder.java
@@ -0,0 +1,13 @@
+public class NumberAdder {
+    public static void main(String[] args) {
+    // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+        System.out.println(addNumber(20));
+    }
+
+    public static int addNumber (int n){
+        if (n == 1){
+            return 1;
+        }
+        return addNumber(n-1)+n;
+    }
+}
