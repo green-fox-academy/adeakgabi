396ee3dda3b8428fa888f69e102d859ec9200d91
Deák Gabriella <adeakgabi@gmail.com>
2019-02-05 14:33:41 +0100

    created Student Class & Main method

--- week-04/day-2/src/greenfoxinheritance/Person.java
+++ week-04/day-2/src/greenfoxinheritance/Person.java
@@ -5,6 +5,14 @@ public class Person {
     int age;
     String gender;
 
+    public void introduce(){
+        System.out.println("Hi, I'm" + name + ", a " + age + " year old "+  gender);
+    }
+
+    public void getGoal(){
+        System.out.println("My goal is: Live for the moment!");
+    }
+
     public Person(String name, int age, String gender){
         this.name = name;
         this.age = age;
@@ -15,14 +23,6 @@ public class Person {
         this.name = "Jane Doe";
         this.age = 30;
         this.gender = "female";
-    }
-
-    public void introduce(){
-        System.out.println("Hi, I'm" + name + ", a " + age + " year old "+  gender);
-    }
-
-    public void getGoal(){
-        System.out.println("My goal is: Live for the moment!"S);
     }
 
 }
