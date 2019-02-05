0847a1bf5c4ec999cd0450340e4bf2c187b4de6d
Deák Gabriella <adeakgabi@gmail.com>
2019-02-05 14:48:03 +0100

    created Sponsor Class

---
+++ week-04/day-2/src/greenfoxinheritance/Sponsor.java
@@ -0,0 +1,31 @@
+package greenfoxinheritance;
+
+public class Sponsor extends Person {
+    String company;
+    int hiredStudents;
+
+    public void introduce(){
+        System.out.println("Hi, I'm " + name + " , a " + age + " year old " + gender + "who represents " + company + " and hired " + hiredStudents + "students so far.");
+    }
+
+    public void hire(){
+        hiredStudents +=1;
+    }
+
+    public void getGoal(){
+        System.out.println("Hire brilliant junior software developers.");
+    }
+
+    public Sponsor(String name, int age, String gender, String company){
+        super(name, age, gender);
+        hiredStudents = 0;
+    }
+
+    public Sponsor(){
+        super();
+        this.company = "Google";
+        hiredStudents = 0;
+    }
+
+
+}
