f692285c32d4e3800a59fdb994dfdaa6904171a2
Deák Gabriella <adeakgabi@gmail.com>
2019-02-07 15:42:19 +0100

    Created Main method & first test for Hashmap - CountLetters Dictionary

--- week-04/day-4/src/CountLetters.java
+++ week-04/day-4/src/CountLetters.java
@@ -1,19 +1,23 @@
 import java.util.HashMap;
 
 public class CountLetters {
+    String word;
 
-    public HashMap<Character, Integer> getDictionary(String useThisString){
+    public CountLetters(){
+        word = "ascdaasdasvbenwne";
+    }
+
+    public HashMap<Character, Integer> getDictionary(String useThisString) {
         HashMap<Character, Integer> stringDictionary = new HashMap<>();
         for (int i = 0; i < useThisString.length(); i++) {
-            if(stringDictionary.containsKey(useThisString.charAt(i))){
+            if (stringDictionary.containsKey(useThisString.charAt(i))) {
                 int temp = stringDictionary.get(useThisString.charAt(i));
                 temp += 1;
                 stringDictionary.put(useThisString.charAt(i), temp);
             } else {
                 stringDictionary.put(useThisString.charAt(i), 1);
             }
-
         }
-        return getDictionary(useThisString);
+        return stringDictionary;
     }
 }
---
+++ week-04/day-4/src/MainForHashmap.java
@@ -0,0 +1,8 @@
+public class MainForHashmap {
+    public static void main(String[] args) {
+        CountLetters word = new CountLetters();
+        System.out.println(word.getDictionary("sfasfadsvdbfdadmlbfd"));
+
+    }
+
+}
