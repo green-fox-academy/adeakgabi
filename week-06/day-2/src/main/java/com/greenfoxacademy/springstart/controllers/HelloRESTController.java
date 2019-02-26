08b89438b1015b5db282cd3bf782b1a65218b850
Deák Gabriella <adeakgabi@gmail.com>
2019-02-26 14:44:52 +0100

    HelloRESTController greeting method counting how many times the page was loaded

--- week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,13 +1,17 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloRESTController {
+    AtomicLong atomicLong = new AtomicLong();
 
     @RequestMapping(value = "/greeting")
-    public Greeting greeting(){
-      return new Greeting(1, "Hello World");
+    public Greeting greeting(@RequestParam ("name") String name){
+      return new Greeting(atomicLong.incrementAndGet(),"Hello " + name);
     }
 }
