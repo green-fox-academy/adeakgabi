8effdeb6c7fe9bc735e01c8de25a19477a5773d5
Deák Gabriella <adeakgabi@gmail.com>
2019-02-26 16:13:53 +0100

    created WebGreetCounter Class to keep HelloWebController's function

--- week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,18 +3,14 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
-    AtomicLong atomicLong = new AtomicLong();
 
     @RequestMapping("/web/greeting")
-    public String greeting(Model model, @RequestParam("name") String name) {
-        model.addAttribute("name", name);
-        model.addAttribute("message", "This site was loaded " + atomicLong.incrementAndGet() + " times since last server start.");
+    public String greeting(Model model) {
+        model.addAttribute("name", " World");
+        model.addAttribute("message", "(It is a web page.)");
 
         return "greeting";
     }
