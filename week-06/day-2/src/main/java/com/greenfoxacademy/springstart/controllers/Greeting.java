fab470ccee24520dfd5e08cd155e6727c0559254
Deák Gabriella <adeakgabi@gmail.com>
2019-02-26 12:27:07 +0100

    created Spring Boot environment

---
+++ week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.springstart.controllers;
+
+public class Greeting {
+    long ID;
+    String content;
+
+    public Greeting(long ID, String content){
+        this.ID = ID;
+        this.content = content;
+    }
+
+    public long getID (){
+        return this.ID;
+    }
+
+    public String getContent(){
+        return this.content;
+    }
+}
