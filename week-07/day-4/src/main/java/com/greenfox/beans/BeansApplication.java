eaad98b97381b1293907f9bf93c26cf54709c298
Deák Gabriella <adeakgabi@gmail.com>
2019-03-07 11:41:08 +0100

    changes in bankofsimba

---
+++ week-07/day-4/src/main/java/com/greenfox/beans/BeansApplication.java
@@ -0,0 +1,26 @@
+package com.greenfox.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeansApplication implements CommandLineRunner {
+
+    private Printer printer;
+
+    @Autowired
+    BeansApplication(Printer printer) {
+        this.printer = printer;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeansApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        printer.log("hello");
+    }
+}
