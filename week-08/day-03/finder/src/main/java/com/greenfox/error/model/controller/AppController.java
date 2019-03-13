68301559c6ee2480104479d9a2399d5f27af77df
Deák Gabriella <adeakgabi@gmail.com>
2019-03-13 15:32:55 +0100

    small changes in finder application

---
+++ week-08/day-03/finder/src/main/java/com/greenfox/error/model/controller/AppController.java
@@ -0,0 +1,30 @@
+package com.greenfox.error.model.controller;
+
+import com.greenfox.error.model.service.UserService;
+import com.greenfox.error.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/app")
+public class AppController {
+
+    @Autowired
+    UserService service;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("new_user", new User());
+        model.addAttribute("yolo", service.getAll());
+        return "index";
+    }
+
+    @RequestMapping("/app")
+    public String create() {
+        service.save(new User());
+        return "redirect:/";
+    }
+}
