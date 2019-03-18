b571e2f7f1818d1382f1ec887e264627ebf81356
Deák Gabriella <adeakgabi@gmail.com>
2019-03-18 13:35:36 +0100

    Frontend excercise environment created

---
+++ week-09/day-1/src/main/java/com/greenfox/frontendexcercise/FrontendexcerciseApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.frontendexcercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendexcerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendexcerciseApplication.class, args);
+    }
+
+}
