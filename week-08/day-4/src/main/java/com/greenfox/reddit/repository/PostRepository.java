945461905e5e6f50977a337ac626b1ea66daea44
Deák Gabriella <adeakgabi@gmail.com>
2019-03-21 09:53:54 +0100

    deleted maven project

--- week-08/day-4/src/main/java/com/greenfox/reddit/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.greenfox.reddit.repository;
-
-import com.greenfox.reddit.model.Post;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<Post, Long> {
-}
