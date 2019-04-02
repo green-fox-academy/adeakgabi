0740e296ba196c4dddfb3c7ccb3dcc9efff13a86
Deák Gabriella <adeakgabi@gmail.com>
2019-04-02 14:29:18 +0200

    create project environment

---
+++ ProjectPhase/week-00/day-02/src/main/java/com/greenfox/springadv/SpringAdvApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.springadv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvApplication.class, args);
+    }
+
+}
