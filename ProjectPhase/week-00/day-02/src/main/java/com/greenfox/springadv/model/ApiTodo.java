09e5acf493248a59539c07a56c568ae7e04d9fad
Deák Gabriella <adeakgabi@gmail.com>
2019-04-02 14:54:21 +0200

    create ApiTodo model and ApiTodoRepository

---
+++ ProjectPhase/week-00/day-02/src/main/java/com/greenfox/springadv/model/ApiTodo.java
@@ -0,0 +1,56 @@
+package com.greenfox.springadv.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ApiTodo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+    private String description;
+    private boolean urgent;
+
+    public ApiTodo(String title, String description, boolean urgent) {
+        this.title = title;
+        this.description = description;
+        this.urgent = urgent;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isUrgent() {
+        return urgent;
+    }
+
+    public void setUrgent(boolean urgent) {
+        this.urgent = urgent;
+    }
+}
---
+++ ProjectPhase/week-00/day-02/src/main/java/com/greenfox/springadv/repository/ApiTodoRepository.java
@@ -0,0 +1,9 @@
+package com.greenfox.springadv.repository;
+
+import com.greenfox.springadv.model.ApiTodo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApiTodoRepository extends CrudRepository<Long, ApiTodo> {
+}
