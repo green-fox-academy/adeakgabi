7913fa453dee4ec790c486e4467444f5f704683b
Deák Gabriella <adeakgabi@gmail.com>
2019-04-03 17:41:30 +0200

    add test method

--- ProjectPhase/week-00/day-02/src/test/java/com/greenfox/springadv/RestControllerTest.java
+++ ProjectPhase/week-00/day-02/src/test/java/com/greenfox/springadv/RestControllerTest.java
@@ -1,19 +1,27 @@
 package com.greenfox.springadv;
 
 import com.greenfox.springadv.controller.MyRestController;
+import com.greenfox.springadv.model.User;
 import com.greenfox.springadv.service.ApiTodoService;
 import com.greenfox.springadv.service.UserService;
+import com.oracle.webservices.internal.api.message.ContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.mockito.ArgumentMatchers.*;
+import java.nio.charset.Charset;
 
-import static org.mockito.Mockito.when;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(MyRestController.class)
@@ -27,8 +35,17 @@ public class RestControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
     @Test
     public void registerWithNullUsername_returnsErrorMessage() throws Exception {
-        // when(userService.)
+        mockMvc.perform(post("/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"userName\": null, \"password\": \"12345\"}"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message")
+                .value("Please provide a username"));
+
     }
 }
