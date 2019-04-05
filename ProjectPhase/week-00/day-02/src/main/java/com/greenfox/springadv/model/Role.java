d7d346979d9fa8293b1dd0e0faaad1b54b7fc523
Deák Gabriella <adeakgabi@gmail.com>
2019-04-05 15:52:07 +0200

    small changes in role class

--- ProjectPhase/week-00/day-02/src/main/java/com/greenfox/springadv/model/Role.java
+++ ProjectPhase/week-00/day-02/src/main/java/com/greenfox/springadv/model/Role.java
@@ -11,6 +11,8 @@ public class Role {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
+
+  //  @Enumerated(EnumType.STRING)
     private String name;
 
     public Long getId() {
